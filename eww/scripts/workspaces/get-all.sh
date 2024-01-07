d6650c7928abb7ac78609102fe97a39e6a473b13
julius-boettger <julius.btg@proton.me>
2024-01-07 11:53:58 +0100

    make eww workspaces work with hyprsome

--- eww/scripts/workspaces/get-all.sh
+++ eww/scripts/workspaces/get-all.sh
@@ -1,9 +1,11 @@
-#!/usr/bin/env bash
-# from https://wiki.hyprland.org/0.34.0/Useful-Utilities/Status-Bars/#configuration
+#!/bin/sh
+# originally from https://wiki.hyprland.org/0.34.0/Useful-Utilities/Status-Bars/#configuration
+# modified to work with hyprsome workspaces (usually 1-9 and 11-19)
 
-spaces (){
-	WORKSPACE_WINDOWS=$(hyprctl workspaces -j | jq 'map({key: .id | tostring, value: .windows}) | from_entries')
-	seq 1 10 | jq --argjson windows "${WORKSPACE_WINDOWS}" --slurp -Mc 'map(tostring) | map({id: ., windows: ($windows[.]//0)})'
+spaces() {
+	LAST=9
+	WORKSPACE_WINDOWS=$(hyprctl workspaces -j | jq 'map({key: .id | tostring, value: (.windows > 0)}) | from_entries')
+	(seq 1 $LAST && seq 11 $((10+$LAST))) | jq --argjson occupied "${WORKSPACE_WINDOWS}" --slurp -Mc 'map(tostring) | map({id: ., occupied: ($occupied[.]//false)})'
 }
 
 spaces
