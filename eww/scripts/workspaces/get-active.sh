d81b37cb5620b77dd5f088512bb8998af95e08fd
julius-boettger <julius.btg@proton.me>
2024-01-09 16:46:11 +0100

    make eww workspaces remember last workspace per monitor

--- eww/scripts/workspaces/get-active.sh
+++ eww/scripts/workspaces/get-active.sh
@@ -1,7 +1,30 @@
 #!/bin/sh
-# from https://wiki.hyprland.org/0.34.0/Useful-Utilities/Status-Bars/#configuration
+# originally from https://wiki.hyprland.org/0.34.0/Useful-Utilities/Status-Bars/#configuration
+# modified to work with hyprsome workspaces (usually 1-9 and 11-19)
 
-hyprctl monitors -j | jq '.[] | select(.focused) | .activeWorkspace.id'
+process_current() {
+  current=$1
+  # echo json array with current workspace for each monitor, last active first
+  if [ "$current" -gt "$LAST" ]; then
+    current2=$current
+    echo "[$current2, $current1]"
+  else
+    current1=$current
+    echo "[$current1, $current2]"
+  fi
+}
 
-socat -u UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - |
-  stdbuf -o0 awk -F '>>|,' -e '/^workspace>>/ {print $2}' -e '/^focusedmon>>/ {print $3}'
+# last hyprsome workspace
+LAST=9
+# current workspace for each monitor, initialize on first each
+current1=1
+current2=11
+
+process_current $(hyprctl monitors -j | jq '.[] | select(.focused) | .activeWorkspace.id')
+
+socat -u UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | while read -r line; do
+  current=$(echo $line | awk -F '>>|,' -e '/^workspace>>/ {print $2}' -e '/^focusedmon>>/ {print $3}')
+  if [ -n "$current" ]; then # if not empty string
+    process_current $current
+  fi
+done
