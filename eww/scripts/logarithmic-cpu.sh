245613bf3c979f9ba685bcb9227149237f2205fb
julius-boettger <julius.btg@proton.me>
2024-01-10 13:38:46 +0100

    eww performance and cpu accuracy improvements

--- eww/scripts/logarithmic-cpu.sh
+++ eww/scripts/logarithmic-cpu.sh
@@ -5,6 +5,4 @@
 actual=$(vmstat 1 2 | tail -1 | awk '{print $15}' | { read idle; echo $((100 - $idle)); })
 # logarithmic value with base 10 (still integer from 0 to 100)
 offset=1 # add to actual and max (100) so result is never 0
-logarithmic=$(python -c "import math; print(round(math.log10((($actual+$offset)/((100+$offset)/9)+1))*100))")
-
-echo "{\"actual\":$actual,\"logarithmic\":$logarithmic}"
+echo $(python -c "import math; print(round(math.log10((($actual+$offset)/((100+$offset)/9)+1))*100))")
