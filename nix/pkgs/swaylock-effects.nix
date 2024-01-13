0c1b86dbc8ef861121d92d4be528884003022e76
julius-boettger <julius.btg@proton.me>
2024-01-13 15:49:05 +0100

    introduce nerdfont characters to swaylock config

--- nix/pkgs/swaylock-effects.nix
+++ nix/pkgs/swaylock-effects.nix
@@ -1,26 +1,31 @@
+# this uses nerdfont characters for text variables
 { writeShellScriptBin }: writeShellScriptBin "swaylock-effects" ''
 swaylock \
 --daemonize \
 --screenshots \
 --clock \
+--datestr "" \
+   --timestr "" \
+  --text-ver "󱥸" \
+--text-wrong "" \
+--text-clear "󱞱" \
 --indicator \
 --indicator-idle-visible \
---no-unlock-indicator \
 --ignore-empty-password \
 --disable-caps-lock-text \
 --font "FiraCode Nerd Font" \
+--font-size 200 \
 --effect-blur 12x3 \
 --indicator-radius 210 \
 --indicator-thickness 10 \
-        --line-color 262527 \
-  --line-clear-color 262527 \
-    --line-ver-color 262527 \
-  --line-wrong-color 262527 \
-    --separator-color 262527 \
-      --inside-color 1A191A77 \
---inside-clear-color 1A191A77 \
-  --inside-ver-color 1A191A77 \
---inside-wrong-color 1A191A77 \
+        --line-color 26252788 \
+  --line-clear-color 26252788 \
+    --line-ver-color 26252788 \
+  --line-wrong-color 26252788 \
+      --inside-color 26252788 \
+--inside-clear-color 26252788 \
+  --inside-ver-color 26252788 \
+--inside-wrong-color 26252788 \
         --text-color F7F1FF \
   --text-clear-color F7F1FF \
     --text-ver-color F7F1FF \
@@ -28,7 +33,8 @@ swaylock \
         --ring-color 5AD4E6 \
   --ring-clear-color 5AD4E6 \
     --ring-ver-color 5AD4E6 \
-        --bs-hl-color FC618D \
+       --bs-hl-color FC618D \
   --ring-wrong-color FC618D \
-      --key-hl-color 948AE3
+      --key-hl-color 948AE3 \
+   --separator-color 403e42
 ''
