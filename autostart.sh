f8724aea1da1e226ce16cec72073d9c6b371d644
julius-boettger <julius.btg@proton.me>
2024-03-02 10:54:31 +0100

    fix noisetorch setup in autostart

--- autostart.sh
+++ autostart.sh
@@ -55,9 +55,18 @@ liquidctl --match kraken initialize
 liquidctl --match kraken set pump speed 30 55 45 100
 
 ### set up virtual microphone with noise reduction
-# only execute if noisetorch is not yet active (grep finds 1 result)
-if [ "$(wpctl status | grep -c 'NoiseTorch Microphone')" -eq 1 ]; then
-  noisetorch -i # init
-  # get wireplumber id of noisetorch mic and set as default
-  wpctl set-default $(wpctl status | grep "NoiseTorch Microphone" | sed "s/[^0-9]*\([0-9]\{2\}\).*/\1/")
+set_mic() {
+  # set $1 as default mic with 100% volume using wireplumber id
+  id=$(wpctl status | sed '/Sources:/,$!d' | grep -m 1 "$1" | sed "s/[^0-9]*\([0-9]\{2\}\).*/\1/")
+  wpctl set-default $id
+  wpctl set-volume $id 100%
+}
+# only execute if noisetorch is not yet active (grep finds <= 1 results)
+if [ "$(wpctl status | grep -c 'NoiseTorch Microphone')" -le 1 ]; then
+  # default mic (source) to apply noise reduction to
+  #set_mic "Scarlett Solo (3rd Gen.) Input 1 Mic"
+  # init noisetorch for default mic
+  noisetorch -i
+  # set noisetorch mic as default
+  set_mic "NoiseTorch Microphone"
 fi
