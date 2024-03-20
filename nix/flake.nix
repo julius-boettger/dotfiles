29c752e5c8c89aa6c3878a86ddb53da5a9929a3d
julius-boettger <julius.btg@proton.me>
2024-03-20 20:38:40 +0100

    add home-manager to flake

--- nix/flake.nix
+++ nix/flake.nix
@@ -4,10 +4,11 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    home-manager.url = github:nix-community/home-manager?ref=nixos-23.11;
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager }:
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs {
@@ -29,6 +30,7 @@
         modules = [
           # shorten this?
           ({ config, pkgs, ... }: { nixpkgs.overlays = [ (final: prev: { unstable = pkgs-unstable; }) ]; })
+          home-manager.nixosModules.home-manager
           ./configuration.nix
         ];
       };
