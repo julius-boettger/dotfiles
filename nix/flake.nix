e66a2d98d93cde72ea3af86eba01da56e744e22c
julius-boettger <julius.btg@proton.me>
2024-03-20 20:42:29 +0100

    add home-manager version

--- nix/flake.nix
+++ nix/flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager?ref=release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
