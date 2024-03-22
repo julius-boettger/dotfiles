fa241449726c4f9a7efb330bb5f3662fb317db18
julius-boettger <julius.btg@proton.me>
2024-03-22 16:02:00 +0100

    add note for wsl in secrets

--- nix/secrets.nix
+++ nix/secrets.nix
@@ -1,6 +1,6 @@
 # this file contains config variables whose actual
 # values i do not want to publish publicly.
 {
-    # tcp port for barrier
+    # tcp port for barrier (irrelevant on WSL)
     barrier.port = 20000;
 }
