2ba3152674e8a42ffb0bf6c8ce594a74ff0f4a67
julius-boettger <julius.btg@proton.me>
2024-05-01 15:32:27 +0200

    clean up autostart.sh

--- other/autostart.sh
+++ other/autostart.sh
@@ -8,17 +8,6 @@ run_once() {
     fi
 }
 
-# only run commands if they are not running,
-# but specify the process name to search for
-run_once_with_pname() {
-    # if there is no process running under the name [first argument]
-    if ! pgrep -f "$1"; then
-        ### execute all arguments except for the first one
-        shift 1 # remove first argument
-        "$@" & # execute all arguments
-    fi
-}
-
 ### only run some commands on xorg/wayland
 # check if number of arguments is >= 1
 if [ $# -ge 1 ]; then
@@ -28,6 +17,7 @@ if [ $# -ge 1 ]; then
     printf "awful=require('awful')\nawful.screen.focus(1)" | awesome-client &
     run_once unclutter --start-hidden --jitter 0 --timeout 3
     run_once picom --experimental-backend
+    run_once nm-applet
     run_once flameshot # not necessary, but makes startup faster
   elif [ "$1" = "wayland" ]; then
     ### only run on wayland
@@ -38,7 +28,6 @@ fi
 ### run display server agnostic commands
 # run programs in background
 run_once lxpolkit
-run_once nm-applet
 
 # running with "run_once" is kind of not reliable +
 # has built-in prevention for running more than once
