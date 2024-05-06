a0329c52a6eeedf23b1d4579f2cbaf5b1279d276
julius-boettger <julius.btg@proton.me>
2024-05-06 09:37:26 +0200

    fix battery script on devices without battery

--- eww/scripts/battery.sh
+++ eww/scripts/battery.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
 charge=$(cat /sys/class/power_supply/BAT0/capacity)
+if [[ $? != 0 ]]; then
+  charge="100"
+fi
 
 status=$(cat /sys/class/power_supply/BAT0/status)
+if [[ $? != 0 ]]; then
+  status="Charging"
+fi
+
 if [[ "$status" == "Charging" ]]; then
   charging="true"
 else
