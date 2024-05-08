2d9a704c0ae856ed12e72f22178f80adbdc831b1
julius-boettger <julius.btg@proton.me>
2024-05-08 16:37:11 +0200

    remove eduroam config

--- nix/modules/laptop-utils.nix
+++ nix/modules/laptop-utils.nix
@@ -1,16 +1,7 @@
 # useful config / tools for laptops
 args@{ pkgs, variables, secrets, ... }:
 {
-  networking.wireless.enable = true;
-
   # enable touchpad support
   services.xserver.libinput.enable = true;
 
-  # eduroam wifi authentication
-  networking.wireless.networks.eduroam.auth = ''
-    key_mgmt=WPA-EAP
-    eap=PWD
-    identity="${secrets.eduroam.email}"
-    password="${secrets.eduroam.password}"
-  '';
 }
