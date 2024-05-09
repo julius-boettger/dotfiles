5c39344aec1c6e1419362df1dd9a3cc8941d2790
julius-boettger <julius.btg@proton.me>
2024-05-09 17:46:42 +0200

    implement network widget script

--- eww/scripts/network.sh
+++ eww/scripts/network.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 
-echo '{"internet":true,"wired":false,"signal_strength":80}'
+# TODO check if internet is reachable
+internet="true"
+
+# get wired connection
+nmcli device | grep ethernet | grep connected > /dev/null
+if [[ $? == 0 ]]; then
+  wired="true"
+  signal_strength=100
+else
+  wired="false"
+  # try to get wifi signal strength
+  signal_strength=$(nmcli device wifi | grep strength | awk '{print $NF}')
+fi
+
+# fallback value for signal strength
+if [ -z "${signal_strength}" ]; then
+  signal_strength=0
+fi
+
+echo "{\"internet\":$internet,\"wired\":$wired,\"signal_strength\":$signal_strength}"
