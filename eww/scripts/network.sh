280dcaf79753336c00af11ae08a77a75e0b576a7
julius-boettger <julius.btg@proton.me>
2024-05-09 21:49:21 +0200

    also display wifi name in widget

--- eww/scripts/network.sh
+++ eww/scripts/network.sh
@@ -3,7 +3,7 @@
 # try to reach internet (google dns server)
 ping -q -c 1 -W 1 8.8.8.8 &> /dev/null
 if [[ $? != 0 ]]; then
-  echo '{"internet":false,"wired":false,"signal_strength":0}'
+  echo '{"internet":false,"wired":false,"signal_strength":0,"wifi_name":""}'
   exit
 fi
 
@@ -14,7 +14,8 @@ if [[ $? == 0 ]]; then
   signal_strength=100
 else
   wired="false"
-  # try to get wifi signal strength
+  # get wifi name and signal strength
+  wifi_name=$(echo $(nmcli d | grep wifi | grep connected | sed 's/^.*connected\s*//'))
   signal_strength=$(nmcli d wifi | grep "^*       " | awk '{print $8}')
   # if signal strength still unknown => 0
   if [ -z "$signal_strength" ]; then
@@ -22,4 +23,4 @@ else
   fi
 fi
 
-echo "{\"internet\":true,\"wired\":$wired,\"signal_strength\":$signal_strength}"
+echo "{\"internet\":true,\"wired\":$wired,\"signal_strength\":$signal_strength,\"wifi_name\":\"$wifi_name\"}"
