a63f9d1c8f89c07a1be25bddc68030a466b1981f
julius-boettger <julius.btg@proton.me>
2024-06-07 11:22:45 +0200

    remove broken firefox icon changes

--- nix/base/gui/default.nix
+++ nix/base/gui/default.nix
@@ -101,16 +101,7 @@ args@{ pkgs, variables, device, script, script-file, ... }:
     libsForQt5.qt5.qtgraphicaleffects
     # icon themes
     gnome.adwaita-icon-theme # just having this installed fixes issues with some apps
-    ((papirus-icon-theme.override { /*folder-*/color = "black"; })
-      # replace default firefox icons with firefox developer edition icons
-      .overrideAttrs (attrs: { postInstall = (attrs.postInstall or "") + ''
-        ln -sf $out/share/icons/Papirus/16x16/apps/firefox-developer-icon.svg $out/share/icons/Papirus/16x16/apps/firefox.svg
-        ln -sf $out/share/icons/Papirus/22x22/apps/firefox-developer-icon.svg $out/share/icons/Papirus/22x22/apps/firefox.svg
-        ln -sf $out/share/icons/Papirus/24x24/apps/firefox-developer-icon.svg $out/share/icons/Papirus/24x24/apps/firefox.svg
-        ln -sf $out/share/icons/Papirus/32x32/apps/firefox-developer-icon.svg $out/share/icons/Papirus/32x32/apps/firefox.svg
-        ln -sf $out/share/icons/Papirus/48x48/apps/firefox-developer-icon.svg $out/share/icons/Papirus/48x48/apps/firefox.svg
-        ln -sf $out/share/icons/Papirus/64x64/apps/firefox-developer-icon.svg $out/share/icons/Papirus/64x64/apps/firefox.svg
-    ''; }))
+    (papirus-icon-theme.override { /*folder-*/color = "black"; })
 
     ### cli
     alsa-utils # control volume
