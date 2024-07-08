2f0dc415c1e095ad0b4c156e71eb6e76cba1e938
julius-boettger <julius.btg@proton.me>
2024-07-08 22:24:02 +0200

    improve hyprctl-collect-clients

--- scripts/hyprctl-collect-clients.sh
+++ scripts/hyprctl-collect-clients.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
-# move all hyprland clients to a single workspace
+# move all hyprland clients to the current workspace
 
-# workspace to move clients to
-target_workspace=11
+target_workspace=$(hyprctl monitors -j | jq '.[] | select(.focused == true) | .activeWorkspace.id')
 
 # result string for hyptctl --batch
-# also switch to target workspace and first workspace for other monitor
-result="dispatch workspace 1; dispatch workspace $target_workspace;"
+result=""
 
 # for every hyprland client address
 for address in $(hyprctl clients -j | jq -r ".[] | select (.workspace.id != $target_workspace and .workspace.id != -1) | .address"); do
