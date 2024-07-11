25bfcd7e3870fe3f3b70e71dfce784a6a6ab2d1c
julius-boettger <julius.btg@proton.me>
2024-07-11 22:02:59 +0200

    allow elevating eww desktop widget

--- eww/scripts/open-everywhere.sh
+++ eww/scripts/open-everywhere.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 # open eww desktop widget on every monitor recognized by hyprland
+# will open widget in background if called without arguments.
+# will toggle between elevation states if called with argument "toggle".
+
+if [ "$#" -eq 1 ] && [ "$1" = "toggle" ]; then
+    # opposite of current state
+    elevate=$(hyprctl layers -j | jq -c '[ to_entries | .[].value.levels."1"[] | select(.namespace == "eww-desktop-widget") | 0 ] != []')
+else
+    elevate="false"
+fi
 
 eww close-all
 
@@ -8,5 +17,5 @@ hyprctl monitors -j | jq -c '.[]' | while read -r monitor; do
     model=$(echo "$monitor" | jq -r '.model')
     name=$(echo "$monitor" | jq -r '.name')
 
-    eww open desktop --screen "$model" --id "$name" --arg "monitor=$name"
+    eww open desktop --screen "$model" --id "$name" --arg "monitor=$name" --arg "elevate=$elevate"
 done
