93a5076903a9898c1d1ca49673859564201b62db
julius-boettger <julius.btg@proton.me>
2024-07-21 09:35:06 +0200

    fix eww-open-everywhere

--- eww/scripts/open-everywhere.sh
+++ eww/scripts/open-everywhere.sh
@@ -16,13 +16,9 @@ fi
 hyprctl monitors -j | jq -c '.[] | {"name": .name, "model": .model}' | while read -r monitor; do
     name=$(jq -r '.name' <<< "$monitor")
 
-    # open in background
     eww open desktop \
         --screen "$(jq -r '.model' <<< "$monitor")" \
         --id "$name" \
         --arg "monitor=$name" \
-        --arg "elevate=$elevate" &
+        --arg "elevate=$elevate"
 done
-
-# wait for background tasks to terminate
-wait
