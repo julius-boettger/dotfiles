1854d93e660bafef29378f8884eb4776a5a00ba0
julius-boettger <julius.btg@proton.me>
2024-09-08 22:36:03 +0200

    move console config from gui to cli base

--- modules/base/cli/default.nix
+++ modules/base/cli/default.nix
@@ -2,11 +2,19 @@
 args@{ pkgs, variables, device, ... }:
 {
   # self-explaining one-liners
-  console.keyMap = "de";
   time.timeZone = "Europe/Berlin";
   nixpkgs.config.allowUnfree = true;
   system.stateVersion = variables.version;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  console = {
+    earlySetup = true;
+    keyMap = "de";
+    colors = [
+      "000000" "FC618D" "7BD88F" "FD9353" "5AA0E6" "948AE3" "5AD4E6" "F7F1FF"
+      "99979B" "FB376F" "4ECA69" "FD721C" "2180DE" "7C6FDC" "37CBE1" "FFFFFF"
+    ];
+  };
 
   networking = {
     hostName = device.hostName;
