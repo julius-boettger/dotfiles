ce21cb33585c7f01dba72dd43af5f52cac559413
julius-boettger <julius.btg@proton.me>
2024-09-08 23:20:04 +0200

    make amount of spaces per tab consistent

--- secrets.nix
+++ secrets.nix
@@ -1,6 +1,6 @@
 # this file contains config variables whose actual
 # values i do not want to publish publicly.
 {
-    # tcp port for barrier (irrelevant on WSL)
-    barrier.port = null;
+  # tcp port for barrier (irrelevant on WSL)
+  barrier.port = null;
 }
