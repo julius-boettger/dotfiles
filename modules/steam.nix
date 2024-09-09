cf92bcc693d962470454283aa36f65ef8005b141
julius-boettger <julius.btg@proton.me>
2024-09-09 12:21:02 +0200

    extract steam config to own module

---
+++ modules/steam.nix
@@ -0,0 +1,11 @@
+# steam (PROPRIETARY)
+args@{ config, lib, pkgs, ... }:
+lib.mkModule "steam" config {
+  programs.steam = {
+    enable = true;
+    package = pkgs.unstable.steam;
+  };
+
+  # easy ge-proton setup for steam
+  environment.systemPackages = [ pkgs.protonup-qt ];
+}
