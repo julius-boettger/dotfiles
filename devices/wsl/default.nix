11734260fc4e23d722731b2aea50dfd302b5d644
julius-boettger <julius.btg@proton.me>
2024-09-09 20:23:04 +0200

    extract wsl-vpnkit config to own module

--- devices/wsl/default.nix
+++ devices/wsl/default.nix
@@ -1,27 +1,16 @@
 args@{ pkgs, variables, ... }:
 {
-  local.gitnuro.enable = true; # (yes, that works with wsl)
-
   wsl = {
     enable = true;
     defaultUser = variables.username;
   };
 
-  environment.systemPackages = with pkgs; [
-    unstable.wsl-vpnkit # for issues with company vpn
-  ];
-
-  # connect vscode on windows to nixos wsl
-  # https://github.com/nix-community/nixos-vscode-server/issues/70
-  services.vscode-server = {
-    enable = true;
-    nodejsPackage = pkgs.nodePackages_latest.nodejs;
-  };
-
-  # for issues with company network
+  # for issues with company network/vpn
   wsl.wslConf.network.generateResolvConf = false;
-  networking.nameservers = [ "8.8.4.4" "8.8.8.8" ];
-  boot.tmp.cleanOnBoot = true;
+  local.wsl-vpnkit.enable = true;
+
+  # git gui (yes, that works with wsl!)
+  local.gitnuro.enable = true;
 
   # automatic garbage collection to free space
   nix.gc = {
@@ -30,20 +19,10 @@ args@{ pkgs, variables, ... }:
     options = "--delete-old";
   };
 
-  # for issues with company vpn
-  environment.shellAliases = {
-    vpn-status =      "systemctl status wsl-vpnkit | sed -n '/Active: /s/Active: //p' | awk '{$1=$1};1'";
-    vpn-start  = "sudo systemctl start  wsl-vpnkit";
-    vpn-stop   = "sudo systemctl stop   wsl-vpnkit";
-  };
-  systemd.services.wsl-vpnkit = {
+  # connect vscode on windows to nixos wsl
+  # https://github.com/nix-community/nixos-vscode-server/issues/70
+  services.vscode-server = {
     enable = true;
-    description = "wsl-vpnkit";
-    serviceConfig = {
-      ExecStart = "${pkgs.unstable.wsl-vpnkit}/bin/wsl-vpnkit";
-      Type = "idle";
-      Restart = "always";
-      KillMode = "mixed";
-    };
+    nodejsPackage = pkgs.nodePackages_latest.nodejs;
   };
 }
