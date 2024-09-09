7babe1df31440b41f93f79b24f8189597da662e0
julius-boettger <julius.btg@proton.me>
2024-09-09 14:02:34 +0200

    extract fastfetch config to own module

---
+++ modules/fastfetch.nix
@@ -0,0 +1,16 @@
+# neofetch but fast
+args@{ config, lib, pkgs, variables, device, ... }:
+let
+  configPath = /etc/dotfiles/devices/${device.internalName}/fastfetch;
+in
+lib.mkModule "fastfetch" config {
+  environment.systemPackages = [ pkgs.fastfetch ];
+
+  # shell alias for shorter fastfetch (which uses device-specific config)
+  environment.shellAliases.fastfetch-short = "fastfetch -c ${configPath}/short.jsonc";
+
+  # symlink device-specific config to ~/.config
+  home-manager.users.${variables.username} = { config, ... }: {
+    xdg.configFile."fastfetch/config.jsonc".source = config.lib.file.mkOutOfStoreSymlink "${configPath}/default.jsonc";
+  };
+}
