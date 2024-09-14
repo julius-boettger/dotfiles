2d299d937f5753c107c9f91ba4cfd33aab652e53
julius-boettger <julius.btg@proton.me>
2024-09-14 17:04:17 +0200

    configure static ip for raspberry pi

--- devices/raspberry-pi/default.nix
+++ devices/raspberry-pi/default.nix
@@ -1,3 +1,18 @@
 {
   imports = [ ../../modules/base/raspberry-pi.nix ];
+
+  # static ipv4 address over ethernet
+  networking = {
+    interfaces.end0.ipv4.addresses = [ {
+      address = "192.168.178.254";
+      prefixLength = 24;
+    } ];
+    # copied from automatically configured values
+    nameservers = [ "192.168.178.1" ];
+    defaultGateway = {
+      address = "192.168.178.1";
+      interface = "end0";
+      metric = 100;
+    };
+  };
 }
