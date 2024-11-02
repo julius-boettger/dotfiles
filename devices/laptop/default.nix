61f54384d4150bccbda50653b0da1b86594a4c1a
julius-boettger <julius.btg@proton.me>
2024-11-02 16:30:00 +0100

    configure autologin

--- devices/laptop/default.nix
+++ devices/laptop/default.nix
@@ -10,6 +10,12 @@ in
   # monitor config with xrandr command
   services.xserver.displayManager.setupCommands = "${pkgs.xorg.xrandr}/bin/xrandr --output eDP --mode 1920x1200 --rate 120";
 
+  # autologin hyprland
+  services.displayManager.sddm.settings.Autologin = {
+    User = variables.username;
+    Session = "hyprland.desktop";
+  };
+
   boot = {
     # boot animation
     plymouth.enable = true;
