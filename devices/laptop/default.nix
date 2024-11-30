bc996bf4c8c86b60150dca71ce645765bd36453c
julius-boettger <julius.btg@proton.me>
2024-11-30 18:12:39 +0100

    improve amd gpu initrd loading

--- devices/laptop/default.nix
+++ devices/laptop/default.nix
@@ -9,5 +9,5 @@ args@{ pkgs, ... }:
   services.xserver.displayManager.setupCommands = "${pkgs.xorg.xrandr}/bin/xrandr --output eDP --mode 1920x1200 --rate 120";
 
   # amd gpu
-  boot.initrd.kernelModules = [ "amdgpu" ];
+  hardware.amdgpu.initrd.enable = true;
 }
