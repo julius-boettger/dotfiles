93fecf0092e663fb8a42ac5a8e69668ca3e9d48e
julius-boettger <julius.btg@proton.me>
2025-01-07 19:20:44 +0100

    improve script to mount encrypted data partition

--- devices/desktop/mount-data.sh
+++ devices/desktop/mount-data.sh
@@ -8,15 +8,30 @@ passphrase=$(zenity --entry --title="Mount encrypted data partition" --text="Ent
 # exit if prompt was canceled
 if [[ $? != 0 ]]; then exit; fi
 
-# try to unlock partition
-echo -n $passphrase | pkexec cryptsetup luksOpen /dev/$partition data
+# gui prompt password
+password=$(zenity --password)
+# exit if prompt was canceled
+if [[ $? != 0 ]]; then exit; fi
+# test if password is correct
+echo -n $password | sudo -S true
 if [[ $? != 0 ]]; then
-    dunstify "mounting failed" "at luks unlock of encrypted data partition"
+    dunstify "mount failed" "password incorrect"
+    exit
+fi
+# sudo should now just work for some time,
+# as it was executed succesfully once
+
+# try to unlock partition
+echo -n $passphrase | sudo cryptsetup luksOpen /dev/$partition data
+if [[ $? != 0 ]]; then
+    dunstify "mount failed" "passphrase of encrypted data partition incorrect"
     exit
 fi
 
 # try to mount partition
-pkexec mount /dev/mapper/data /mnt/data
+sudo mount /dev/mapper/data /mnt/data
 if [[ $? != 0 ]]; then
-    dunstify "mounting failed" "at mount of encrypted data partition"
+    dunstify "mount failed" "at actual mount of unlocked partition"
 fi
+
+dunstify "mount successful" "of encrypted data partition"
