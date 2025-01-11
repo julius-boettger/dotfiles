eb995b6abc324b14ccc0cbc0e8d301337e0b56bc
julius-boettger <julius.btg@proton.me>
2025-01-11 11:33:30 +0100

    fix noisetorch autostart

--- misc/autostart.sh
+++ misc/autostart.sh
@@ -37,7 +37,7 @@ python /etc/dotfiles/misc/update/update_on_saturday.py &
 ### set up virtual microphone with noise reduction
 set_mic() {
   # set $1 as default mic with 100% volume using wireplumber id
-  id=$(wpctl status | sed '/Sources:/,$!d' | grep -m 1 "$1" | sed "s/[^0-9]*\([0-9]\{2\}\).*/\1/")
+  id=$(wpctl status | sed '/Sources:/,$ { /Streams:/q; p }' | grep -m 1 "$1" | sed "s/[^0-9]*\([0-9]\{2,\}\).*/\1/")
   if [ -z "$id" ]; then
     echo "couldnt get id for microphone '$1'"
     return
@@ -47,6 +47,8 @@ set_mic() {
 }
 # only execute if noisetorch is not yet active (grep finds <= 1 results)
 if [ "$(wpctl status | grep -c 'NoiseTorch Microphone')" -le 1 ]; then
+  # wait for mics to be detected correctly
+  sleep 1
   # default mic (source) to apply noise reduction to
   set_mic "Scarlett Solo (3rd Gen.) Input 1 Mic"
   # init noisetorch for default mic
