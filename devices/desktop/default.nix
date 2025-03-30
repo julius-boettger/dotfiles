145dc19942c2ba42ccc198442d4e276e3f92327a
julius-boettger <julius.btg@proton.me>
2025-03-30 17:23:25 +0200

    remove obsolete config

--- devices/desktop/default.nix
+++ devices/desktop/default.nix
@@ -10,8 +10,6 @@ args@{ config, pkgs, variables, ... }:
     playerctl.enable = true;
   };
 
-  boot.supportedFilesystems.ntfs = true;
-
   # for focusrite usb audio interface (get with `dmesg | grep Focusrite`)
   boot.extraModprobeConfig = "options snd_usb_audio vid=0x1235 pid=0x8211 device_setup=1";
 
