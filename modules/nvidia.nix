2da931a03933ffaabf258924dc9095c9e1220806
julius-boettger <julius.btg@proton.me>
2025-05-30 22:39:26 +0200

    update flake.lock (and fix nvidia drivers)

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -14,6 +14,15 @@ lib.mkModule "nvidia" config {
       openSha256 = "sha256-2DpY3rgQjYFuPfTY4U/5TcrvNqsWWnsOSX0f2TfVgTs=";
       settingsSha256 = "sha256-5m6caud68Owy4WNqxlIQPXgEmbTe4kZV2vZyTWHWe+M=";
       persistencedSha256 = "sha256-OSo4Od7NmezRdGm7BLLzYseWABwNGdsomBCkOsNvOxA=";
+      patches = [(
+        # https://github.com/NVIDIA/open-gpu-kernel-modules/issues/840
+        pkgs.fetchpatch {
+          url = "https://github.com/CachyOS/kernel-patches/raw/914aea4298e3744beddad09f3d2773d71839b182/6.15/misc/nvidia/0003-Workaround-nv_vm_flags_-calling-GPL-only-code.patch";
+          hash = "sha256-YOTAvONchPPSVDP9eJ9236pAPtxYK5nAePNtm2dlvb4=";
+          stripLen = 1;
+          extraPrefix = "kernel/";
+        }
+      )];
     };
   };
 
