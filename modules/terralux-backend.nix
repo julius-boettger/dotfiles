b653e1fcd36c916e4a56b65844eb16e4975b0d79
julius-boettger <julius.btg@proton.me>
2025-06-08 12:44:52 +0200

    update terralux-backend

--- modules/terralux-backend.nix
+++ modules/terralux-backend.nix
@@ -1,20 +1,13 @@
 # server to control smart plug for reptile terrarium lamp
 args@{ config, lib, variables, device, ... }:
 let
-  internalPort = 5000; # currently hard-coded in server
-  externalPort = 5001; # only exposed to local net
+  port = 5000; # currently hard-coded in server
   terralux-backend-pkg = (lib.getPkgs "terralux-backend").terralux-backend;
 in 
 lib.mkModule "terralux-backend" config {
 
   environment.systemPackages = [ terralux-backend-pkg ];
-  networking.firewall.allowedTCPPorts = [ externalPort ];
-
-  local.website.extraConfig = ''
-    http://:${toString externalPort} {
-      reverse_proxy :${toString internalPort}
-    }
-  '';
+  networking.firewall.allowedTCPPorts = [ port ];
 
   systemd.services.terralux-backend = {
     enable = true;
