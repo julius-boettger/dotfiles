72c9fca28ef5cb735398d22df8996416fcb6f8a7
julius-boettger <julius.btg@proton.me>
2025-06-25 14:11:31 +0200

    update gitnuro

--- modules/gitnuro/default.nix
+++ modules/gitnuro/default.nix
@@ -2,10 +2,10 @@
 args@{ config, lib, pkgs, ... }:
 lib.mkModule "gitnuro" config {
   environment.systemPackages = [ (pkgs.unstable.gitnuro.overrideAttrs (attrs: rec {
-    version = "1.4.2";
+    version = "1.5.0";
     src = pkgs.fetchurl {
       url = "https://github.com/JetpackDuba/Gitnuro/releases/download/v${version}/Gitnuro-linux-x86_64-${version}.jar";
-      hash = "sha256-1lwuLPR6b1+I2SWaYaVrZkMcYVRAf1R7j/AwjQf03UM=";
+      hash = "sha256-EoBjw98O5gO2wTO34KMiFeteryYapZC83MUfIqxtbmQ=";
     };
   })) ];
 }
