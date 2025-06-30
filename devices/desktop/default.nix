ea401daaf50b9225ae8a3131e4b22327ed624fc7
julius-boettger <julius.btg@proton.me>
2025-06-30 20:01:20 +0200

    add more swap on desktop

--- devices/desktop/default.nix
+++ devices/desktop/default.nix
@@ -7,6 +7,12 @@ args@{ config, pkgs, variables, ... }:
     playerctl.enable = true;
   };
 
+  # more swap, couldnt figure out how to change it using disko/btrfs
+  swapDevices = [{
+    device = "/.swapfile2";
+    size = 16*1024; # 16 GB
+  }];
+
   # for focusrite usb audio interface (get with `dmesg | grep Focusrite`)
   boot.extraModprobeConfig = "options snd_usb_audio vid=0x1235 pid=0x8211 device_setup=1";
 
