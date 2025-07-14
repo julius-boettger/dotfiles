bb55cf3b5000f588ee9c648ab09a4ffd2d94892e
julius-boettger <julius.btg@proton.me>
2025-07-14 13:27:55 +0200

    modify system packages

--- modules/studies/default.nix
+++ modules/studies/default.nix
@@ -3,8 +3,6 @@
   # not all modules, just the ones I currently use
   imports = [];
 
-  local.virtualbox.enable = true;
-
   programs.ssh.extraConfig = ''
     Host gitlab.on.hs-bremen.de gitlab
       Hostname gitlab.on.hs-bremen.de
