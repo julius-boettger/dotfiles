f3f8755e9fd33dc466b1c43f10035bf310dd27f2
julius-boettger <julius.btg@proton.me>
2025-08-02 16:36:03 +0200

    modify system packages

--- modules/steam.nix
+++ modules/steam.nix
@@ -3,7 +3,7 @@ args@{ config, lib, pkgs, ... }:
 lib.mkModule "steam" config {
   programs.steam = {
     enable = true;
-    package = pkgs.unstable.steam;
+    package = pkgs.steam;
   };
 
   # easy ge-proton setup for steam
