95a0839353212828e30ccc15e20acdbd51b20d24
julius-boettger <julius.btg@proton.me>
2026-06-08 22:02:12 +0200

    modify system packages

--- modules/studies.nix
+++ modules/studies.nix
@@ -2,6 +2,7 @@
 args@{ config, lib, pkgs, ... }:
 lib.mkModule "studies" config {
   #local.devtools.docker.enable = true;
+  local.virt-manager.enable = true;
 
   environment.systemPackages = with pkgs; [
     zotero
