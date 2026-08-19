eb1100fec8b21317d503e614f7c09ff840ac7024
julius-boettger <julius.btg@proton.me>
2026-08-19 20:24:20 +0200

    upgrade gitnuro to v2.0.0 beta 1

--- modules/gitnuro/default.nix
+++ modules/gitnuro/default.nix
@@ -1,11 +1,15 @@
 # git gui
 args@{ config, lib, pkgs, ... }:
 lib.mkModule "gitnuro" config {
-  environment.systemPackages = [ (pkgs.gitnuro/*.overrideAttrs (attrs: rec {
-    version = "1.5.0";
-    src = pkgs.fetchurl {
-      url = "https://github.com/JetpackDuba/Gitnuro/releases/download/v${version}/Gitnuro-linux-x86_64-${version}.jar";
-      hash = "sha256-EoBjw98O5gO2wTO34KMiFeteryYapZC83MUfIqxtbmQ=";
-    };
-  })*/) ];
+  environment.systemPackages = [
+    ((pkgs.gitnuro.override {
+      jre = pkgs.jdk25;
+    }).overrideAttrs (attrs: rec {
+      version = "2.0.0-beta01";
+      src = pkgs.fetchurl {
+        url = "https://github.com/JetpackDuba/Gitnuro/releases/download/${version}/Gitnuro-linux-x86_64-2.0-beta01-2.0.0.jar";
+        hash = "sha256-DAvZdKV82mYa09WiBnxVRgb8XucWg3UQjXEc7CO8gO8=";
+      };
+    }))
+  ];
 }
